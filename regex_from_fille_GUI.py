7d140792197cce55a3f2abf1c519976e80304719
Damian Małkowski <atre99@gmail.com>
2019-02-16 15:58:13 +0100

    Add files via upload

---
+++ regex_from_fille_GUI.py
@@ -0,0 +1,33 @@
+# Wyszukuje w otwartym tekscie wyrazy "głowa,ręka, ucho" i umieszcza je w liście
+import re
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Text('Wpisz proszę słowa których szukasz w pliku.')],
+    [sg.FileBrowse(), sg.InputText('Plik')],
+    [sg.Text('Szukane słowo: ', size=(15, 1)), sg.InputText('')],
+    [sg.Submit(), sg.Cancel()]
+]
+
+window = sg.Window('Ile razy występuje w tekstcie dane słowo.').Layout(layout)
+button, values, = window.Read()
+
+
+def regex(txt):
+    book_regex = re.compile(values[1], re.IGNORECASE)
+    mo = book_regex.findall(txt)
+    num = 0
+    for i in mo:
+        num += 1
+    sg.Popup('Słowo '+str(values[1])+' występuje '+str(num)+' razy.')
+
+
+filename = values[0]
+try:
+    with open(filename, encoding='utf8') as f_obj:
+        content = f_obj.read()
+except FileNotFoundError:
+    print('Plik '+filename+' nie został znaleziony')
+
+
+regex(content)
